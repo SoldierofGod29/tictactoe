c164a8cf5047181c0008619553a38c5801770848
SoldierofGod29 <nzollers@hotmail.com>
2024-05-17 22:04:43 -0500

    Add javascript file to html; Add modules for player, gameboard, and game Logic; Add functions to player to get and set the players piece

--- javascript.js
+++ javascript.js
@@ -1,2 +1,52 @@
 //JavaScript Code for Tic Tac Toe Project
 
+var player = (function() {
+    var player1 = "";
+    var player2 = "";
+
+    function setPlayers() {
+        let userInput = prompt("X or O")
+
+        if (userInput == "X")
+        {
+            player1 = "X";
+            player2 = "O";
+        }
+        else if (userInput == "O")
+        {
+            player1 = "O";
+            player2 = "X";
+        }
+        else
+        {
+            userInput = prompt("X or O");
+        }
+    }
+
+    function getPlayer1() {
+        return player1;
+    }
+
+    function getPlayer2(){
+        return player2;
+    }
+
+    return {
+        getPlayer1: getPlayer1,
+        getPlayer2: getPlayer2,
+        setPlayers: setPlayers,
+    }
+})()
+
+var gameBoard = (function() {
+    var gameBoardArray = [];
+    
+});
+
+var gameLogic = (function() {
+
+});
+
+player.setPlayers();
+console.log(player.getPlayer1());
+console.log(player.getPlayer2());

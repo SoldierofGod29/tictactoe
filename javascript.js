d7b079cee473c5d75606c28306c690198448f760
SoldierofGod29 <nzollers@hotmail.com>
2024-05-18 21:56:58 -0500

    Add functions to get data from the array at a certain position, Add some functions to gameLogic objective to get a random number, the users choice, computers choice, conditions for winning the game, and one to play the game

--- javascript.js
+++ javascript.js
@@ -45,6 +45,10 @@ var gameBoard = (function() {
         gameBoardArray[position] = piece;
     }
 
+    function getPosition (position) {
+        return gameBoardArray[position];
+    }
+
     function checkPosition (position) {
         if (gameBoardArray[position] == null)
             {
@@ -103,10 +107,194 @@ var gameBoard = (function() {
         printArray: printArray,
         checkPosition: checkPosition,
         placePieceInArray: placePieceInArray,
+        getPosition: getPosition
     }
 })();
 
 var gameLogic = (function() {
 
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 9);
+    }
     
+    function getUserInput() {
+        let userInput = prompt("Choose a position 0-8");
+        
+        if (gameBoard.checkPosition(userInput) == false)
+            {
+                return userInput;
+            }
+        else if (gameBoard.checkPosition(userInput) == true)
+            {
+                alert("Position "+userInput+" has already been taken!");
+                userInput = prompt("Choose a position 0-8");
+            } 
+    }
+
+    function getComputerInput() {
+        let computerInput = getRandomNumber();
+        let i = true;
+
+        while (i = true)
+            {
+                if (gameBoard.checkPosition(computerInput) == true)
+                    {
+                        computerInput = getRandomNumber();
+                    }
+                else
+                    {
+                        return computerInput;
+                    }
+            }
+    }
+
+    function winCondition() {
+        if ((gameBoard.getPosition(0) == gameBoard.getPosition(1) && gameBoard.getPosition(0) == gameBoard.getPosition(2)) && gameBoard.getPosition(0) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(0))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(0))
+                    {
+                        return "player2";
+                    }
+            }
+
+        else if ((gameBoard.getPosition(0) == gameBoard.getPosition(3) && gameBoard.getPosition(0) == gameBoard.getPosition(6)) && gameBoard.getPosition(0) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(0))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(0))
+                    {
+                        return "player2";
+                    }
+            }
+        else if ((gameBoard.getPosition(0) == gameBoard.getPosition(4) && gameBoard.getPosition(0) == gameBoard.getPosition(8)) && gameBoard.getPosition(0) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(0))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(0))
+                    {
+                        return "player2";
+                    }
+            }
+        else if ((gameBoard.getPosition(1) == gameBoard.getPosition(4) && gameBoard.getPosition(1) == gameBoard.getPosition(7)) && gameBoard.getPosition(1) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(1))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(1))
+                    {
+                        return "player2";
+                    }
+            }
+
+        else if ((gameBoard.getPosition(2) == gameBoard.getPosition(4) && gameBoard.getPosition(2) == gameBoard.getPosition(6)) && gameBoard.getPosition(2) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(2))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(2))
+                    {
+                        return "player2";
+                    }
+            }
+        else if ((gameBoard.getPosition(2) == gameBoard.getPosition(5) && gameBoard.getPosition(2) == gameBoard.getPosition(8)) && gameBoard.getPosition(2) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(2))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(2))
+                    {
+                        return "player2";
+                    }
+            }
+        else if ((gameBoard.getPosition(3) == gameBoard.getPosition(4) && gameBoard.getPosition(3) == gameBoard.getPosition(5)) && gameBoard.getPosition(3) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(3))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(3))
+                    {
+                        return "player2";
+                    }
+            }
+        else if ((gameBoard.getPosition(6) == gameBoard.getPosition(7) && gameBoard.getPosition(6) == gameBoard.getPosition(8)) && gameBoard.getPosition(6) != null)
+            {
+                if (player.getPlayer1() == gameBoard.getPosition(6))
+                    {
+                        return "player1";
+                    }
+                else if (player.getPlayer2() == gameBoard.getPosition(6))
+                    {
+                        return "player2";
+                    }
+            }
+    }
+
+    function playGame() {
+        player.setPlayers();
+        let i = 0;
+         while(i < 5)
+            {
+                gameBoard.placePieceInArray(player.getPlayer1(), getUserInput());
+                if (i != 4)
+                    {
+                        gameBoard.placePieceInArray(player.getPlayer2(), getComputerInput());
+                    }
+                
+
+                gameBoard.printArray();
+                console.log("-----------------");
+
+                if (winCondition() == "player1")
+                    {
+                        console.log("Player 1 wins!");
+                        break;
+                    }
+                else if (winCondition() == "player2")
+                    {
+                        console.log("Player 2 wins!");
+                        break;
+                    }
+
+                console.log(i);
+                if (i == 4)
+                    {
+                        if (winCondition() == "player1")
+                            {
+                                console.log("Player 1 wins!");
+                                break;
+                            }
+                        else if (winCondition() == "player2")
+                            {
+                                console.log("Player 2 wins!");
+                                break;
+                            }
+                        else 
+                            {
+                                console.log("It's a tie");
+                                break;
+                            }
+                    }
+                
+                        
+                i++;
+            }
+    }
+
+    return {
+        playGame: playGame,
+    }
 })();
+
+gameLogic.playGame();
